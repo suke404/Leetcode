e9239d137825861c5fab22dde2f938399f3a636d
suke404 <mc3chan2003@gmail.com>
2023-07-20 16:08:40 +0900

    Leetcode -338 Counting Bits

--- .vscode/Leetcode -190 Reverse Bits.cpp
+++ .vscode/Leetcode -190 Reverse Bits.cpp
@@ -3,11 +3,12 @@ public:
     uint32_t reverseBits(uint32_t n) {
         int newN = 0;
         for(int i = 0; i < 32; i++) {
-            newN = newN << 1;
-            if((n & 1) > 0) {
-                newN = newN ^ 1;
+            newN = newN << 1; // shift left by 1
+            if((n & 1) > 0) { 
+                // bitwise & > 0, if it is 1 and 0
+                newN = newN ^ 1; // bitwise or 0 1
             }
-            n = n >> 1;
+            n = n >> 1; // right shift
         }
         return newN;
     }
